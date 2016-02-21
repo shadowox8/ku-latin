46c335390d64d40b72c76921d1455af9c41279b7
C. Watson <christopher@keithwatson.com>
2016-02-20 22:12:48 -0600

    macronStrip 2.0

--- macronStrip.js
+++ macronStrip.js
@@ -1,14 +1,27 @@
+// 100% Sure-Proof.
+// Inspired by: http://stackoverflow.com/questions/990904/remove-accents-diacritics-in-a-string-in-javascript
+
 /**
- * Replaces macron'd characters with their LOWERCASE non-macron'd equivalent.
+ * Replaces LOWERCASE macron'd characters with their non-macron'd equivalent.
  * @param {string} str The string to be stripped.
  * @returns {string} The stripped string.
  */
 function macronStrip(str) {
-	// str = str.toLowerCase();
-	str = str.replace(/ā/gi, "a");
-	str = str.replace(/ē/gi, "e");
-	str = str.replace(/ī/gi, "i");
-	str = str.replace(/ō/gi, "o");
-	str = str.replace(/ū/gi, "u");
-	return str;
+	// JSLint demanded that I use this.
+	"use strict";
+
+	// It seems silly to run this each time the function is invoked, but it
+	// seems equally silly to make this a global variable... what do?
+	var map = [];
+	map["ā"] = "a";
+	map["ē"] = "e";
+	map["ī"] = "i";
+	map["ō"] = "o";
+	map["ū"] = "u";
+
+	// Finds macron'd letters and replaces them with their non-macron'd
+	// equivalents as defined by `map`.
+	return str.replace(/[āēīōū]/g, function (match) {
+		return map[match];
+	});
 }
